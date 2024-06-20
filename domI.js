a1c594a8ba39fb7416a519528333f125a069d70d
Adriasu <adriana.suarez@students.gammatech.school>
2024-06-20 13:31:32 +0200

    terminado

--- domI.js
+++ domI.js
@@ -2,16 +2,19 @@ for (let i = 1; i <= 100; i++) {
   const button = document.createElement("button");
   document.querySelector("section").appendChild(button);
   button.innerHTML = i;
-  button.style.width = "80px"
-  button.style.height = "80px"
-  i % 2 === 0 ? button.style.backgroundColor = "blue" : button.style.backgroundColor = "pink"
+  button.style.width = "80px";
+  button.style.height = "80px";
+  i % 2 === 0
+    ? (button.style.backgroundColor = "blue")
+    : (button.style.backgroundColor = "pink");
   if (i % 5 === 0) {
-    button.style.color = "green"
+    button.style.color = "green";
   }
 }
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
-
-
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const spans = document.querySelectorAll("div > span");
+for (let i = 0; i < spans.length; i++) {
+  spans[i].style.color = colors[i]
+}
 
